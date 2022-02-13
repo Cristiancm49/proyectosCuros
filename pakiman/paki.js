54192f3c51484980bda4061b90db33d43582fbc9
Cristian Cortes Mondragon <ccortesm2000@gmail.com>
2022-02-13 05:16:12 -0500

    Se agrtego nuevo personaje lobo en el codigo

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -2,17 +2,23 @@ var imagenes= [];
 imagenes["Cauchin"] = "vaca.png";
 imagenes["Pokacho"] = "pollo.png";
 imagenes["Tocinaurio"] = "cerdo.png";
+imagenes["perrauro"] = "perro.png";
+imagenes["lbomin"] = "lobo.png";
 
 
 var cauchin = new Pakiman("Cauchin", 120, 50);
 var pokacho = new Pakiman ("Pokacho", 140, 50);
 var tocinaurio = new Pakiman("Tocinaurio", 140, 60);
+var lbomin = new Pakiman("Lobomani", 150, 50);
+var perrauro = new Pakiman("Perrauro", 160, 50);
 
 
 var coleccion = [];
 coleccion.push(new Pakiman("Cauchin", 100, 30));
 coleccion.push(new Pakiman ("Pokacho", 80, 50));
 coleccion.push(new Pakiman("Tocinaurio", 120, 40));
+coleccion.push(new Pakiman("Lobomani", 150, 50));
+coleccion.push(new Pakiman("Perrauro", 160, 50));
 
 for (var pakin of coleccion) {
   pakin.mostrar();
